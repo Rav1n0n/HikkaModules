5574803143ed11e9bd45e2d390a0fa341b4a1f5e
Rav1n0n <rav1non@yandex.ru>
2024-01-22 22:36:24 +0500

    Add files via upload

---
+++ Scratch Komaru.py
@@ -0,0 +1,20 @@
+#я еблан и разраб этой хуйни: телеграм @y9RavenCat
+from hikkatl.types import Message
+from .. import loader 
+from hikka import utils
+
+url = "https://x0.at/OLui.mp4"
+
+@loader.tds
+class ScrKomMod(loader.Module):
+    """Scratch Komaru
+    Developer: @y9RavenCat"""
+    strings = {"name": "Scratch Komaru", "caption": "ВЫ ПОЧЕСАЛИ КОМАРУ"}
+    
+    @loader.command(
+        ru_doc="Почеши Комару"
+        # ...
+    )
+    async def scrkom(self, message: Message):
+        """- Вы почешите Комару ;)"""
+        await utils.answer_file(message, file=url, caption=self.strings("caption"))
