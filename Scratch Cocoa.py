7818e34aca79c758de909edda89e5758dea117f4
Rav1n0n <rav1non@yandex.ru>
2024-01-22 22:36:45 +0500

    Add files via upload

---
+++ Scratch Cocoa.py
@@ -0,0 +1,20 @@
+#я еблан и разраб этой хуйни: телеграм @y9RavenCat
+from hikkatl.types import Message
+from .. import loader 
+from hikka import utils
+
+url = "https://x0.at/urag.mp4"
+
+@loader.tds
+class ScratchCocoaMod(loader.Module):
+    """Scratch Cocoa
+    Developer: @y9RavenCat"""
+    strings = {"name": "Scratch Cocoa", "caption": "ВЫ ПОЧЕСАЛИ КОКОА"}
+    
+    @loader.command(
+        ru_doc="- Вы почешите Кокоа ;)"
+        # ...
+    )
+    async def scrcoca(self, message: Message):
+        """- Вы почешите Кокоа ;)"""
+        await utils.answer_file(message, file=url, caption=self.strings("caption"))
