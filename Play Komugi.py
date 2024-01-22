c26feb74aaa00ef77f1cebb0b9dd2dfb571d487f
Rav1n0n <rav1non@yandex.ru>
2024-01-22 22:51:24 +0500

    Update Play Komugi.py

--- Play Komugi.py
+++ Play Komugi.py
@@ -6,15 +6,15 @@ from hikka import utils
 url = "https://x0.at/0T5N.mp4"
 
 @loader.tds
-class PlayKomugiMod(loader.Module):
+class PlayWithKomugiMod(loader.Module):
     """Play with Komugi
     Developer: @y9RavenCat"""
-    strings = {"name": "Play Komugi", "caption": "ВЫ ПОИГРАЛИ С КОМУГИ"}
+    strings = {"name": "Play With Komugi", "caption": "ВЫ ПОИГРАЛИ С КОМУГИ"}
     
     @loader.command(
         ru_doc="Поиграй с Комуги"
         # ...
     )
-    async def plkomug(self, message: Message):
+    async def plkomugi(self, message: Message):
         """- Вы поиграете с комуги ;)"""
         await utils.answer_file(message, file=url, caption=self.strings("caption"))
